0ad080f660926bfc4bc14e18dd26104e288f2001
yu.khmelevskikh <yu.khmelevskikh@robo.cash>
2022-05-24 15:43:21 +0300

    Created domain layer models

---
+++ app/src/main/java/com/example/changecolor/domain/PaintSettings.kt
@@ -0,0 +1,6 @@
+package com.example.changecolor.domain
+
+data class PaintSettings(
+    val isLeftSidePainted: Boolean = false,
+    val isRightSidePainted: Boolean = false
+)
---
+++ app/src/main/java/com/example/changecolor/domain/RgbColor.kt
@@ -0,0 +1,7 @@
+package com.example.changecolor.domain
+
+data class RgbColor(
+    val red: Int = 0,
+    val green: Int = 0,
+    val blue: Int = 0
+)
